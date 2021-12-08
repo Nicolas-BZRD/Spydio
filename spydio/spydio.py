c4562ea8eb4f2cf9c00f57027d334b785d777f1f
NICOLAS BZRD <79255399+Nicolas-BZRD@users.noreply.github.com>
2021-12-08 20:53:28 +0100

    Rotation function

--- spydio/spydio.py
+++ spydio/spydio.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from numpy.lib.function_base import gradient
 import scipy.io.wavfile as spiow
 from SOFASonix import SOFAFile
 from scipy import signal
@@ -9,12 +10,14 @@ class WavSpydio:
         if path != None:
             self.song_sr, self.song = spiow.read(path)
             self.song = np.asarray(self.song)
-            self.duration = len(self.song)/self.song_sr
+            self.spDuration = len(self.song)
+            self.duration = self.spDuration/self.song_sr
             self.dim = self.song[0].size
         else:
             self.song_sr = kwargs["song_sr"]
             self.song = kwargs["song"]
-            self.duration = len(self.song)/self.song_sr
+            self.spDuration = len(self.song)
+            self.duration = self.spDuration/self.song_sr
             self.dim = self.song[0].size
 
 class Spydio:
@@ -27,10 +30,10 @@ class Spydio:
 
     def spatialize(self, wavSpydio:WavSpydio, azimuth:int, elevation:int=4, channel:int=0):
         if elevation > 8 or elevation < 0: elevation = 4
-        if azimuth < 0 or azimuth > 360: azimuth = abs(azimuth//360)
+        if azimuth < 0 or azimuth > 360: azimuth = azimuth%360
 
-        if azimuth == 0 or azimuth == 360: azimuth = 0
-        else: azimuth = round((360-azimuth)/5)
+        if azimuth == 360: azimuth = 0
+        else: azimuth = (360-azimuth)//5
 
         if wavSpydio.dim == 1:
             processedL = np.array(signal.fftconvolve(wavSpydio.song[:], self.HRIR[(72*elevation)+azimuth][0][:]))
@@ -40,7 +43,77 @@ class Spydio:
             processedR = np.array(signal.fftconvolve(wavSpydio.song[:,1], self.HRIR[(72*elevation)+azimuth][1][:]))
 
         song = np.array([processedL, processedR]).transpose()
-        return WavSpydio(path=None, song=song, song_sr=wavSpydio.song_sr)        
+        return WavSpydio(path=None, song=song, song_sr=wavSpydio.song_sr)
+
+    def rotation(self, wavSpydio:WavSpydio, azimuthStart:int, azimuthEnd:int, elevationStart:int=4, elevationEnd:int=4):
+        azimuthStart = 5*round(azimuthStart/5)
+        azimuthEnd = 5*round(azimuthEnd/5)
+
+        azimuthVariation = azimuthEnd - azimuthStart
+        azimuthEnd = azimuthEnd+5 if azimuthVariation > 0 else azimuthEnd-5
+
+        azimuth = azimuthStart
+        elevation = elevationStart
+        spatialized = []
+
+        while azimuth != azimuthEnd:
+            spatialized.append(self.spatialize(wavSpydio, azimuth, elevation))
+            azimuth = azimuth+5 if azimuthVariation > 0 else azimuth-5
+
+        srVariation = wavSpydio.spDuration//(azimuthVariation//5)
+        leftChannel = []
+        rightChannel = []
+        for i in range(0, azimuthVariation//5):
+            if(i==0):
+                gate = self.gradientGate(srVariation-512, srVariation+512, 1, 0, spDuration=spatialized[i].spDuration)
+                leftChannel = spatialized[i].song[:,0]*gate
+                rightChannel = spatialized[i].song[:,1]*gate
+            elif(i+1==azimuthVariation//5):
+                gate = self.gradientGate((srVariation*i)-512, (srVariation*i)+512, 0, 1, spDuration=spatialized[i].spDuration)
+                leftChannel = np.add(leftChannel, spatialized[i].song[:,0]*gate)
+                rightChannel = np.add(rightChannel, spatialized[i].song[:,1]*gate)
+            else:
+                gate = self.gate( (srVariation*i)-512, (srVariation*i)+512, (srVariation*(i+1))-512, (srVariation*(i+1))+512, spDuration=spatialized[i].spDuration)
+                leftChannel = np.add(leftChannel, spatialized[i].song[:,0]*gate)
+                rightChannel = np.add(rightChannel, spatialized[i].song[:,1]*gate)
+
+        song = np.array([leftChannel, rightChannel]).transpose()
+        return WavSpydio(path=None, song=song, song_sr=wavSpydio.song_sr)
+
+    def gradientGate(self, start:int, end:int, startAmplitude:float, endAmplitude:float, spDuration:int=0):
+        if(start > end):
+            tmp = end
+            end = start
+            start = tmp
+        if(start < 0): start = 0
+        if(end > spDuration): spDuration = end
+
+        if(start == 0 and spDuration-end == 0):
+            attack = np.linspace(startAmplitude,endAmplitude,end-start)
+        elif(start == 0):
+            attack = np.concatenate([np.linspace(startAmplitude,endAmplitude,end-start), np.full((spDuration-end), endAmplitude)])
+        elif(spDuration-end == 0):
+            attack = np.concatenate([np.full((start), startAmplitude), np.linspace(startAmplitude,endAmplitude,end-start)])
+        else:
+            attack = np.concatenate([np.full((start), startAmplitude), np.linspace(startAmplitude,endAmplitude,end-start), np.full((spDuration-end), endAmplitude)])
+        return attack
+
+    def gate(self, startIncrease:int, endIncrease:int, startDecrease:int, endDecrease:int, startAmplitude:float=0, sustainAmplitude:float=1, endAmplitude:float=0, spDuration:int=0):
+        attack = self.gradientGate(0, endIncrease-startIncrease, startAmplitude, endAmplitude=sustainAmplitude)
+        sustain = np.full((startDecrease-endIncrease), sustainAmplitude)
+        decrease = self.gradientGate(0, endDecrease-startDecrease, sustainAmplitude, endAmplitude)
+
+        if(startIncrease != 0 and endIncrease !=spDuration):
+            start = np.full((startIncrease), startAmplitude)
+            end = np.full((spDuration-endDecrease), endAmplitude)
+            gate = np.concatenate([start, attack, sustain, decrease, end])
+        elif(startIncrease != 0):
+            start = np.full((startIncrease), startAmplitude)
+            gate = np.concatenate([start, attack, sustain, decrease])
+        elif(endIncrease !=spDuration):
+            end = np.full((spDuration-endDecrease), endAmplitude)
+            gate = [attack, sustain, decrease, end]
+        return gate
 
     def loadWavFile(self, path:str):
         wav = WavSpydio(path)
